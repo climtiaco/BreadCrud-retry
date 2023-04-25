b8a7dab0e9745e61d091b3de051eaf55ab0cf733
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-04-24 20:03:06 -0700

    sending index and show data

--- breads/server.js
+++ breads/server.js
@@ -12,6 +12,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome app about Breads!');   
 })
 
+//MIDDLEWARE // Setting this up makes it so when we're calling res.render() it'll use the view engine we set up right here.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 //Breads
 const breadsController = require('./controllers/breads_controller');
 app.use('/breads', breadsController);
